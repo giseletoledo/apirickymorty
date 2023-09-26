2fbb36f321d727c111731d872159c67dbef2fdc7
gisele <gisesonia@gmail.com>
2023-09-26 20:19:05 -0300

    Initial Commit

---
+++ rickymorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickymorty
+//
+//  Created by GISELE TOLEDO on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
