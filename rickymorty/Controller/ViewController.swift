42fa3209882a9c771daa15a4aebfb142d2bafe87
gisele <gisesonia@gmail.com>
2023-09-30 14:13:32 -0300

    Implementando a requisicao de API

--- rickymorty/Controller/ViewController.swift
+++ rickymorty/Controller/ViewController.swift
@@ -8,40 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
-    
-    private var arrayRickModel:[RickModel] = []
-    
-    private lazy var homeView:HomeView = {
+
+    private var arrayRickModel: [RickModel] = []
+    var currentPage = 1 // Mantenha o controle da página atual
+
+
+    private lazy var homeView: HomeView = {
         let homeView = HomeView()
         homeView.setTableViewDelegate(self)
-        
         return homeView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
         view = homeView
-        fetchModel()
+
+        // Carregue os dados da API quando a vista é carregada
+        loadMoreData()
+
         homeView.configureProtocols(datasource: self)
         navigationController?.delegate = self
     }
-    
-    fileprivate func fetchModel() {
-        arrayRickModel.append(RickModel(title: "Item 1", status: "Status 1", lastKnownLocation: "Location 1", memories: "Memories 1", imageNames: "image1"))
-        arrayRickModel.append(RickModel(title: "Item 2", status: "Status 2", lastKnownLocation: "Location 2", memories: "Memories 2", imageNames: "image2"))
-        arrayRickModel.append(RickModel(title: "Item 3", status: "Status 3", lastKnownLocation: "Location 3", memories: "Memories 3", imageNames: "image3"))
-        arrayRickModel.append(RickModel(title: "Item 4", status: "Status 4", lastKnownLocation: "Location 4", memories: "Memories 4", imageNames: "image4"))
-        arrayRickModel.append(RickModel(title: "Item 5", status: "Status 5", lastKnownLocation: "Location 5", memories: "Memories 5", imageNames: "image1"))
+
+    // Função para carregar os dados da API
+    func fetchRickModels() async throws -> [RickModel] {
+        // Faça a requisição HTTP aqui
+        let (data, _) = try await URLSession.shared.data(from: URL(string: "https://api.rickandmorty.com/api/character/?page=\(1)")!)
+
+        // Decodifica os dados da resposta
+        let rickModels = try JSONDecoder().decode([RickModel].self, from: data)
+
+        return rickModels
     }
-    
-    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        if viewController == self {
-            // Esta condição verifica se estamos voltando para a lista principal
-            // Você pode atualizar a lista principal aqui, se necessário.
-            homeView.updateTableView()
-        }
+
+    @objc func loadMoreData() {
+        // Faça a requisição HTTP com base na página atual
+            let urlString = "https://api.rickandmorty.com/api/character/?page=\(currentPage)"
+            DispatchQueue.global().async {
+                Task {
+                    do {
+                        let (data, _) = try await URLSession.shared.data(from: URL(string: urlString)!)
+                        let rickModels = try JSONDecoder().decode([RickModel].self, from: data)
+                        
+                        // Adicione os novos dados à matriz existente
+                        self.arrayRickModel += rickModels
+                        
+                        // Atualize a tabela na thread principal
+                        DispatchQueue.main.async {
+                            self.homeView.updateTableView()
+                        }
+                        
+                        // Incremente a página atual para carregar mais dados na próxima vez
+                        self.currentPage += 1
+                    } catch {
+                        // Trate os erros aqui, se necessário
+                        print("Erro ao buscar dados: \(error)")
+                    }
+                }
+            }
     }
 }
 
@@ -49,14 +75,14 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayRickModel.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeViewCell().listViewCellIdentifier) as? HomeViewCell else {
             // Lida com o erro de desembrulhar a célula
             fatalError("A célula personalizada não pode ser desembrulhada.")
         }
-        
+
         // Verifique se o índice está dentro dos limites do array de nomes de imagem
         cell.configure(model: arrayRickModel[indexPath.row])
         cell.backgroundColor = .white
@@ -64,15 +90,13 @@ extension ViewController: UITableViewDataSource {
     }
 }
 
-
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
         // Crie uma instância da DetailViewController com os dados da célula selecionada
         let detailViewController = DetailViewController(selectedModel: arrayRickModel[indexPath.row])
-        
+
         // Apresente a DetailViewController
         navigationController?.pushViewController(detailViewController, animated: true)
     }
 }
-
--- rickymorty/Model/RickModel.swift
+++ rickymorty/Model/RickModel.swift
@@ -5,19 +5,29 @@
 //  Created by GISELE TOLEDO on 27/09/23.
 //
 
-struct RickModel {
+struct RickModel: Decodable {
     let title: String
     let status: String
     let lastKnownLocation: String
     let memories: String
     let imageNames: String
-    
-    init(title: String, status: String, lastKnownLocation: String, memories: String, imageNames: String) {
-        self.title = title
-        self.status = status
-        self.lastKnownLocation = lastKnownLocation
-        self.memories = memories
-        self.imageNames = imageNames
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        title = try container.decode(String.self, forKey: .title)
+        status = try container.decode(String.self, forKey: .status)
+        lastKnownLocation = try container.decode(String.self, forKey: .lastKnownLocation)
+        memories = try container.decode(String.self, forKey: .memories)
+        imageNames = try container.decode(String.self, forKey: .imageNames)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case status
+        case lastKnownLocation
+        case memories
+        case imageNames
     }
 }
 
